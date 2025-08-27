717608f8a4b71f30c9df57f25e492644c646ae7b
MontseMoran <m-moran@hotmail.es>
2025-08-27 17:41:49 +0200

    hamburguer menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,26 @@ import { useTranslation } from "react-i18next"
 import "../styles/Navbar.scss"
 import LanguageSelector from "./LanguageSelector";
 import logo from "/logo.png"
+import { useState } from "react";
 
 function Navbar (){
 const {t} = useTranslation();
+const [isOpen, setIsOpen] = useState(false);
 
 return(
 <nav className="navbar" role="navigation" aria-label="Menú principal">
+<button
+  className="navbar__toggle"
+  onClick={() => setIsOpen(!isOpen)}
+  aria-label="Abrir menú"
+>
+  ☰
+</button>
 
   <div className="navbar__brand">
    <img src={logo} alt="" />
   </div>
-<ul className="navbar__menu">
+<ul className={`navbar__menu ${isOpen ? "open" : ""}`}>
   <li><a className="navbar__link" href="#sobre">{t("nav.about")}</a></li>
   <li><a className="navbar__link" href="#servicios">{t("nav.services")}</a></li>
   <li><a className="navbar__link" href="#beneficios">{t("nav.benefits")}</a></li>
