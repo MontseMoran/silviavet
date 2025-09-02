8d41901287a0dfdca988495f479a762c7e91e362
MontseMoran <m-moran@hotmail.es>
2025-09-02 23:02:02 +0200

    Add cards footer and whatsApp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,33 +4,45 @@ import "../styles/Footer.scss";
 export default function Footer() {
   const { t } = useTranslation();
   const year = new Date().getFullYear();
+
   return (
-    <footer className="footer footer--diagonals">
-      <div className="footer__inner">
+<footer className="footer footer--diagonals">
+  <div className="footer__inner">
 
-        <div className="footer__panel">
-          <div className="footer__contact">
-            <a href="tel:+34613898805" className="footer__link">📞 +34 613 898 805</a>
-            <a href="mailto:info@silviavet.com" className="footer__link">📧 info@silviavet.com</a>
-          </div>
-
-          <div className="footer__social">
-         <span>{t("footer.follow")}:</span>
-            <a
-              href="https://www.facebook.com/SilviaGasperinivet"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Facebook de Silvia Vet"
-            >
-              <img src="/icons/facebook.png" alt="Facebook" />
-            </a>
-          </div>
-        </div>
-
-        <div className="footer__legal">
-              {t("footer.legal", { year })}
-        </div>
+    <div className="footer__cards">
+     
+      <div className="footer__card footer__card--left">
+        <p>Llámame o escríbeme por WhatsApp</p>
+        <a href="https://wa.me/34613898805" target="_blank" rel="noreferrer">
+          <img src="/icons/whatsApp.png" alt="WhatsApp" className="footer__icon"/>
+        </a>
       </div>
-    </footer>
-  );
+
+     
+      <div className="footer__card footer__card--center">
+        <p>Contáctame</p>
+        <a href="tel:+34613898805" className="footer__link">📞 +34 613 898 805</a>
+        <a href="mailto:info@silviavet.com" className="footer__link">📧 info@silviavet.com</a>
+      </div>
+
+    
+      <div className="footer__card footer__card--right">
+        <p>{t("footer.follow")}:</p>
+        <a
+          href="https://www.facebook.com/SilviaGasperinivet"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Facebook de Silvia Vet"
+        >
+          <img src="/icons/facebook.png" alt="Facebook" className="footer__icon"/>
+        </a>
+      </div>
+    </div>
+
+    <div className="footer__legal">
+      {t("footer.legal", { year })}
+    </div>
+  </div>
+</footer>
+  )
 }
